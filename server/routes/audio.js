a013a66b0bd714a6bca28bb76496b0d535ff36b3
srishtigupta075 <srishtigupta075@gmail.com>
2023-08-20 15:21:20 +0530

    remove server

--- server/routes/audio.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { createTranslation } = require("../controllers/audioController");
-const Multer = require("multer");
-
-const upload = Multer();
-router.post("/translate/", upload.single("file"), createTranslation);
-
-module.exports = router;
